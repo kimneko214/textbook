3dd4c3d9da7d99806a13d739f34f392c98d824d1
kimneko214 <w214ddd@gmail.com>
2026-09-21 00:33:54 +0900

    Update books.js

--- books.js
+++ books.js
@@ -8,7 +8,7 @@ const BOOKS = [
     cover: "",
     tags: ["1", "2", "3"]
   },
- {
+  {
     title: "MOS1023 Chapter2",
     author: " ",
     category: " ",
@@ -17,4 +17,157 @@ const BOOKS = [
     cover: "",
     tags: ["1", "2", "3"]
   },
+  {
+    title: "MOS1023 Chapter3",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter3.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter4",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter4.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter5",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter5.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter6",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter6.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter7",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter7.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter8",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter8.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter9",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter9.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter10",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter10.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter11",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter11.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter12",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter12.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter13",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter13.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter14",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter14.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter15",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter15.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter16",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter16.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter17",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter17.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter18",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter18.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  },
+  {
+    title: "MOS1023 Chapter19",
+    author: " ",
+    category: " ",
+    course: "MOS1023",
+    file: "https://pub-002f890c4bc747aaa00746a1da6d6bc4.r2.dev/chapter19.pdf",
+    cover: "",
+    tags: ["1", "2", "3"]
+  }
 ];
